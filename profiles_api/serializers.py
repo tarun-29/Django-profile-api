6b53df0276a5b019b7c3c59df0c111fd0cea027f
tarun-29 <29kantiwaltarun@gmail.com>
2022-08-19 13:56:14 +0530

    final : profile app

---
+++ profiles_api/serializers.py
@@ -0,0 +1,40 @@
+import email
+from wsgiref import validate
+from rest_framework import serializers
+from profiles_api import models
+
+class HelloSerializer(serializers.Serializer):
+  """Serializers a name field for testing our apiview"""
+  name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+  """Serialize a user profile object"""
+
+  class Meta:
+    model = models.UserProfile
+    fields = ('id', 'email', 'name', 'password')
+    extra_kwargs = {
+      'password' : {
+        'write_only': True,
+        'style': {'input_type': 'password'}
+      }
+    }
+
+  def create(self, validated_data):
+    """Create and return new user"""
+    user = models.UserProfile.objects.create_user(
+      email=validated_data['email'],
+      name=validated_data['name'],
+      password = validated_data['password']
+    )
+
+    return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+  """Serializer profile feed item"""
+
+  class Meta:
+    model = models.ProfileFeedItem
+    fields = ('id', 'user_profile', 'status_text', 'created_on')
+    extra_kwargs = {'user_profile': {'read_only': True}}
---
+++ profiles_api/urls.py
@@ -0,0 +1,16 @@
+from django import urls
+from django.urls import path, include
+from profiles_api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
+router.register("profile", views.UserProfileViewSet)
+router.register("feed", views.UserProfileFeedViewSet)
+
+
+urlpatterns = [
+    path('hello-view', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
+    path('', include(router.urls))
+]
